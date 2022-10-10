342ad22da1b30eb2a5518f39922bd9c2d036e4f5
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-10-10 00:35:15 -0300

    add bootstrap

--- src/components/ListarPokemons/ListarPokemons.tsx
+++ src/components/ListarPokemons/ListarPokemons.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {useState} from "react";
+import './style.css';
 
 const ListarPokemonsComponent = () => {
     const [pokemons, setPokemons] = useState([]);
@@ -7,7 +8,9 @@ const ListarPokemonsComponent = () => {
     const [objImgPokemons, setObjImgPokemons] = useState([{}]);
 
     const listarPokemons = async () => {
-        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=3");
+        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=50");
+
+        console.log("result => ", result)
 
         setPokemons(result.data.results);
     };
@@ -26,17 +29,30 @@ const ListarPokemonsComponent = () => {
     };
 
     return (
-        <div>
-            <div>
+        <div className="container-fluid">
+            <div className="container">
+                <button onClick={listarPokemons} className="btn btn-primary">Listar Pokemons</button>
+                <button onClick={listarImgPokemons} className="btn btn-primary">Listar Img Pokemons</button>
+            </div>
+            <div className="row">
                 {
                     objImgPokemons.map((objPokemon: any, index: number) => (
-                        <img key={index} src={objPokemon.img} alt={objPokemon.nome} />
+                        <div className="card shadow-sm col-1">
+                            <div className="card-body shadow">
+                                <img key={index} src={objPokemon.img} title={objPokemon.nome} alt={objPokemon.nome} className="imgPokemon"/>
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">{objPokemon.nome}</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     ))
                 }
-            </div>
-            <div>
-                <button onClick={listarPokemons}>Listar Pokemons</button>
-                <button onClick={listarImgPokemons}>Listar Img Pokemons</button>
             </div>
         </div>
     )
