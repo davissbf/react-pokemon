6f2274f139563ce89992001bd328e2996843c870
Emanuell Santos de Jesus <dfmanu06@gmail.com>
2022-10-10 01:10:31 -0300

    add xp do pokemon

--- src/components/ListarPokemons/ListarPokemons.tsx
+++ src/components/ListarPokemons/ListarPokemons.tsx
@@ -3,25 +3,39 @@ import {useState} from "react";
 import './style.css';
 
 const ListarPokemonsComponent = () => {
-    const [pokemons, setPokemons] = useState([]);
+    const [pokemons, setPokemons] = useState([{}]);
 
     const [objImgPokemons, setObjImgPokemons] = useState([{}]);
 
     const listarPokemons = async () => {
-        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=50");
+        const result = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=100");
 
-        console.log("result => ", result)
+        const arrayPokemons: Array<object> = [];
 
-        setPokemons(result.data.results);
+        for (const objPokemon of result.data.results) {
+            const result2 = await axios.get(objPokemon.url);
+
+            arrayPokemons.push({
+                ...objPokemon,
+                experiencia: result2.data.base_experience
+            });
+        }
+
+        setPokemons(arrayPokemons);
     };
 
     const listarImgPokemons = () => {
         const arrayImgs: Array<object> = [];
 
+        console.log("pokemons => ", pokemons)
+
         pokemons.forEach((pokemon: any, index: number) => {
+            const nomePokemon = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+
             arrayImgs.push({
-                nome: pokemon.name,
-                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+                nome: nomePokemon,
+                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+                experiencia: pokemon.experiencia,
             });
         })
 
@@ -37,17 +51,18 @@ const ListarPokemonsComponent = () => {
             <div className="row">
                 {
                     objImgPokemons.map((objPokemon: any, index: number) => (
-                        <div className="card shadow-sm col-1">
+                        <div className="card shadow-sm col-2">
                             <div className="card-body shadow">
-                                <img key={index} src={objPokemon.img} title={objPokemon.nome} alt={objPokemon.nome} className="imgPokemon"/>
+                                <img key={index} src={objPokemon.img} title={objPokemon.nome} alt={objPokemon.nome} />
                             </div>
                             <div className="card-body">
-                                <p className="card-text">{objPokemon.nome}</p>
+                                <p className="card-text"><b>{objPokemon.nome}</b></p>
                                 <div className="d-flex justify-content-between align-items-center">
                                     <div className="btn-group">
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                        <button type="button" className="btn btn-success">View</button>
+                                        <button type="button" className="btn btn-primary">Edit</button>
                                     </div>
+                                    <small><b>XP: {objPokemon.experiencia}</b></small>
                                 </div>
                             </div>
                         </div>
